f589fd692e80fb4f83b041d9f6ec22efa64715a5
KhushalID7 <23bcp137@sot.pdpu.ac.in>
2025-06-25 12:37:02 +0530

    made YOLO world work text input remaining

--- detect.py
+++ detect.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import cv2
+import supervision as sv
+from inference.models.yolo_world.yolo_world import YOLOWorld
 import numpy as np
 
 st.set_page_config(page_title="Webcam Stream", layout="centered")
@@ -9,6 +11,11 @@ st.text_input("Class of object that you want to detect")
 
 haar_path = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
 face_cascade = cv2.CascadeClassifier(haar_path)
+
+model = YOLOWorld(model_id="yolo_world/l")
+classes = ["person","hand", "backpack", "dog", "eye", "nose","Phone","Headphone","bottle","spectacles"]
+
+
 
 # Initialize session state for camera
 if 'camera_on' not in st.session_state:
@@ -29,10 +36,22 @@ if st.session_state.camera_on:
             break
         frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         frame = cv2.flip(frame, 1)
-        gray = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
-        faces = face_cascade.detectMultiScale(gray, scaleFactor=1.1, minNeighbors=5)
-        for (x, y, w, h) in faces:
-            cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        
+        results = model.infer(frame, text=classes, confidence=0.03)
+        detections = sv.Detections.from_inference(results)
+
+        bounding_box_annotator = sv.BoxAnnotator()
+        label_annotator = sv.LabelAnnotator()
+
+        labels = [classes[class_id] for class_id in detections.class_id]
+
+        annotated_image = bounding_box_annotator.annotate(
+            scene=frame, detections=detections
+        )
+        annotated_image = label_annotator.annotate(
+            scene=annotated_image, detections=detections, labels=labels
+        )
+
         FRAME_WINDOW.image(frame)
         # Add a small delay to avoid high CPU usage
         if not st.session_state.camera_on:
